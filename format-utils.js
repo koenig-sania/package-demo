60ffe2fd4ed5e9a58a24ca38458abe048d1ba918
koenig-sania <121497218+koenig-sania@users.noreply.github.com>
2025-11-28 01:25:59 +0530

    Add capitalize function to format-utils.js

---
+++ format-utils.js
@@ -0,0 +1,15 @@
+// format-utils.js
+/**
+ * Capitalizes the first letter of a given string.
+ * @param {string} str - The input string.
+ * @returns {string} The capitalized string.
+ */
+function capitalize(str) {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+// Export the function so other projects can import it.
+module.exports = {
+  capitalize
+};
